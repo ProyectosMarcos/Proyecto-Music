ab2a6d673627301ef50bd653ce8d35632cf974e0
ProyectosMarcos <m-fernandez988@hotmail.com>
2023-05-22 20:00:06 -0300

    mejoras - perfil - modal y carrousel

--- Proyecto-Musicbox/perfil/app.js
+++ Proyecto-Musicbox/perfil/app.js
@@ -1,33 +1,82 @@
-const controls = document.querySelectorAll(".control");
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
+const slider = document.querySelector('.slider');
+const innerSlider = document.querySelector('.inner-slider');
+const prev = document.querySelector('.slider-prev');
+const next = document.querySelector('.slider-next');
 
-controls.forEach((control) => {
-  control.addEventListener("click", (e) => {
-    isLeft = e.target.classList.contains("arrow-left");
+let dragged = false;
+let startX;
+let x;
 
-    if (isLeft) {
-      currentItem -= 1;
-    } else {
-      currentItem += 1;
-    }
+// Desktop Version
 
-    if (currentItem >= maxItems) {
-      currentItem = 0;
-    }
-
-    if (currentItem < 0) {
-      currentItem = maxItems - 1;
-    }
-
-    items.forEach((item) => item.classList.remove("current-item"));
-
-    items[currentItem].scrollIntoView({
-      behavior: "smooth",
-       inline: "center",
-       block: "nearest"
-     })
-    items[currentItem].classList.add("current-item");
-  });
+slider.addEventListener("mousedown", e => {
+    dragged = true;
+    startX = e.offsetX - innerSlider.offsetLeft;
+    slider.style.cursor = "grabbing";
 });
+
+slider.addEventListener("mouseenter", () => {
+    slider.style.cursor = "grab";
+});
+
+slider.addEventListener("mouseup", () => {
+    slider.style.cursor = "grab";
+    dragged = false;
+});
+
+slider.addEventListener("mousemove", e => {
+    if (!dragged) return;
+    e.preventDefault();
+
+    x = e.offsetX;
+
+    innerSlider.style.left = `${x - startX}px`;
+
+    checkProbs();
+
+});
+
+// Mobile Version
+
+slider.addEventListener('touchstart', e => {
+    dragged = true;
+    startX = e.targetTouches[0].clientX - innerSlider.offsetLeft;
+
+    checkProbs();
+
+}, {passive: true});
+
+slider.addEventListener('touchmove', e => {
+    if (!dragged) return;
+    x = e.targetTouches[0].clientX;
+
+    innerSlider.style.left = `${x - startX}px`;
+
+    checkProbs();
+
+}, {passive: true});
+
+prev.addEventListener('click', () => {
+    let innerSliderLeft = innerSlider.style.left;
+    innerSlider.style.left = parseInt(innerSliderLeft.replace('px', '')) + 265 + 'px';
+
+    checkProbs();
+});
+
+next.addEventListener('click', () => {
+    let innerSliderLeft = innerSlider.style.left;
+    innerSlider.style.left = innerSliderLeft.replace('px', '') - 265 + 'px';
+
+    checkProbs();
+});
+
+const checkProbs = () => {
+    let outer = slider.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if (parseInt(innerSlider.style.left) > 0)
+        innerSlider.style.left = "-10px";
+
+    if (inner.right < outer.right)
+        innerSlider.style.left = `-${inner.width - outer.width - 10}px`;
+}
