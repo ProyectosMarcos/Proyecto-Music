3b1eaf78ec1aeaa0fa468cda35ff822eb856ed92
gaboalvarez <alvarezgabriel907@gmail.com>
2023-06-26 23:37:26 -0300

    DETALLE_LISTA - VUE no modal

---
+++ Proyecto-Musicbox/LISTAS/VUE-DETALLE_LISTA_PROPIA.js
@@ -0,0 +1,15 @@
+const { createApp } = Vue
+createApp({
+ data() {
+ return {
+    nombreLista: "Rock de los 70s",
+    descripcion: "Esta lista contiene las verdaderas canciones del rock",
+    albumes:[
+        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"},
+        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"},
+        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"},
+        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"}
+    ]
+}
+ }
+}).mount('.contenedor-grid')
