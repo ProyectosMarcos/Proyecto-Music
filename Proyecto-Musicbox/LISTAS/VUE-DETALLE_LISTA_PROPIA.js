3f726cf7b50f53fb98e987ff89a4d1f13bb3f929
gaboalvarez <alvarezgabriel907@gmail.com>
2023-06-27 14:57:14 -0300

    DETALLE_LISTA - VUE mejorado

--- Proyecto-Musicbox/LISTAS/VUE-DETALLE_LISTA_PROPIA.js
+++ Proyecto-Musicbox/LISTAS/VUE-DETALLE_LISTA_PROPIA.js
@@ -4,11 +4,8 @@ createApp({
  return {
     nombreLista: "Rock de los 70s",
     descripcion: "Esta lista contiene las verdaderas canciones del rock",
+    cantAlbumes:0,
     albumes:[
-        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"},
-        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"},
-        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"},
-        {nombreAlbum: "The Wall",refAlbum:"/Proyecto-Musicbox/detalle_album/detalle_album.html",nombreArchivoAlbum:"/Proyecto-Musicbox/albums/The_Wall.png"}
     ]
 }
  }
